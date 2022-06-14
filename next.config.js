2b0f71ecee57165f9e5742af7b3b3bd2299d2705
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-06-14 23:52:44 +0200

    remove next config redirect

--- next.config.js
+++ next.config.js
@@ -26,15 +26,3 @@ module.exports = async () => {
     },
   };
 };
-
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/nl',
-        destination: '/nl-nl',
-        permanent: true,
-      },
-    ]
-  },
-}
